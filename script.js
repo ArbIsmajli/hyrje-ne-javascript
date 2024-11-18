1ab4e288e4803b26228113313f96b1046ae556e2
ArbIsmajli <ismajliarb@gmail.com>
2024-11-18 18:42:02 +0100

    punova

---
+++ script.js
@@ -0,0 +1,14 @@
+//this is a one line comment
+//variablat:
+let emri= "mirjeta"; //string
+let notaMatematike = 4; //number
+let x; //undefined
+let isAdult = false; //true
+let nullVariabla = null;
+
+// deklarimi i variables = krijojme nje variabel
+let testVar;
+// inicializim - i variables i japim vlere per here te pare
+testVar = "m";
+// deklarimi + inicializimi ne te njejtin rresht
+const PI = 3.14;
